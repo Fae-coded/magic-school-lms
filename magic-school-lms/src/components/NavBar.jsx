fc203f5ad43de1dd1f2095486486d76423d95605
Sophie Beech <219414866+Fae-coded@users.noreply.github.com>
2026-01-13 16:33:31 +0000

    Add routes and navbar link

--- magic-school-lms/src/components/NavBar.jsx
+++ magic-school-lms/src/components/NavBar.jsx
@@ -1,4 +1,5 @@
 import crest from '../assets/StrixhavenCrest.webp';
+import { Link } from 'react-router-dom';
 
 export default function NavBar() {
     return (
@@ -6,7 +7,7 @@ export default function NavBar() {
             <img src={crest} alt="Strixhaven University Crest" className="navbar-crest" />
             <button className="navbar-button">Your Courses</button>
             <h3 className="navbar-welcome">Welcome Student name</h3>
-            <button className="navbar-logout">Logout</button>
+            <Link to="/login-register" className="navbar-login">Login/Register</Link>
         </div>
     );
 }
--- magic-school-lms/src/index.jsx
+++ magic-school-lms/src/index.jsx
@@ -3,12 +3,29 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
 import Root from './pages/Root.jsx'
+import Home from './pages/Home.jsx';
+import Form from './pages/LoginRegister.jsx';
 import StudentDashboard from './pages/student.jsx'
+import TeacherDashboard from './pages/Teacher.jsx'
+import StudentCourses from './pages/StudentCourses.jsx'
+import CreateCourse from './pages/CreateCourse.jsx';
+import EditCourse from './pages/EditCourse.jsx';
+import AdminDashboard from './pages/Admin.jsx';
+import ManageUsers from './pages/ManageUsers.jsx';
 import App from './App.jsx'
 
 const router = createBrowserRouter(createRoutesFromElements(
   <Route path="/" element={<Root />}>
+    <Route index element={ <Home /> } />
+    <Route path="/login-register" element={ <Form /> } />
     <Route path="/student" element={ <StudentDashboard /> } />
+    <Route path="/enrolled-courses" element={ <StudentCourses /> } />
+    <Route path="/teacher" element={ <TeacherDashboard /> } />
+    <Route path="/create-course" element={ <CreateCourse /> } />
+    <Route path="/edit-course" element={ <EditCourse /> } />
+    <Route path="/admin" element={ <AdminDashboard /> } />
+    <Route path="/manage-users" element={ <ManageUsers /> } />
+  
   </Route>
   
 ));
