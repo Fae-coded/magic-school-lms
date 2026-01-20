5b8557de8719565e0a38ab8077bf883a822e6b19
Sophie Beech <219414866+Fae-coded@users.noreply.github.com>
2026-01-20 15:11:31 +0000

    Build django backend and initial models

---
+++ magic_school_project/server/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+def get_substitute_teacher():
+    # Returns the ID of a substitute teacher when a teacher is deleted.
+    return User.objects.filter(role=User.Role.TEACHER).first().id
+
+class User(AbstractUser):
+    class Role(models.TextChoices):
+        STUDENT = 'student', 'Student'
+        TEACHER = 'teacher', 'Teacher'
+        ADMIN = 'admin', 'Admin'
+    
+    role = models.CharField(
+        max_length=10,
+        choices=Role.choices,
+        default=Role.STUDENT
+    )
+
+class Course(models.Model):
+    course_title = models.CharField(max_length=100)
+    course_description = models.TextField()
+    teacher = models.ForeignKey(User, on_delete=models.SET(get_substitute_teacher), limit_choices_to={'role': User.Role.TEACHER})
+    
+    def __str__(self):
+        return self.course_title
