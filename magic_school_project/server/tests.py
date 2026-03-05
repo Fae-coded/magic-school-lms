472390f182c0d60afc49bb48b1c8200d31b153ba
Sophie Beech <219414866+Fae-coded@users.noreply.github.com>
2026-03-05 12:12:00 +0000

    Add api tests for user and course detail endpoints

--- magic_school_project/server/tests.py
+++ magic_school_project/server/tests.py
@@ -7,9 +7,7 @@ from rest_framework_simplejwt.tokens import RefreshToken
 
 
 class RegisterAPITestCase(APITestCase):
-    def setUp(self):
-        self.url = reverse('register')
-        
+    def setUp(self):        
         self.valid_payload = {
             'username': 'aidoneus',
             'email': 'aidoneus@strixhaven.com',
@@ -20,6 +18,8 @@ class RegisterAPITestCase(APITestCase):
             'username': 'aidoneus',
             'password': 'Odette!69'
         }
+        
+        self.url = reverse('register')
         
     def register_user(self, payload):
         return self.client.post(self.url, payload, format='json')
@@ -55,9 +55,7 @@ class RegisterAPITestCase(APITestCase):
 
 
 class LoginAPITestCase(APITestCase):
-    def setUp(self):
-        self.url = reverse('login')
-        
+    def setUp(self):        
         self.user = User.objects.create_user(
             username='aidoneus',
             email='aidoneus@strixhaven.com',
@@ -73,6 +71,8 @@ class LoginAPITestCase(APITestCase):
             'username': 'aidoneus',
             'password': 'WrongPassword'
         }
+        
+        self.url = reverse('login')
         
     def login_user(self, payload):
         return self.client.post(self.url, payload, format='json')
@@ -104,7 +104,6 @@ class LoginAPITestCase(APITestCase):
         
 class LogoutAPITestCase(APITestCase):
     def setUp(self):
-        self.url = reverse('logout')
         
         self.user = User.objects.create_user(
             username= 'aurelia',
@@ -115,6 +114,8 @@ class LogoutAPITestCase(APITestCase):
         self.refresh = RefreshToken.for_user(self.user)
         
         self.client.force_authenticate(user=self.user)
+        
+        self.url = reverse('logout')
         
     def test_gets_refresh_token_and_blacklists_token(self):
         response = self.client.post(
@@ -128,9 +129,8 @@ class LogoutAPITestCase(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
     
     
-class UserAPITestCase(APITestCase):
+class UserListAPITestCase(APITestCase):
     def setUp(self):
-        self.url = reverse('user-list')
         
         self.admin = User.objects.create_user(
             username= 'adminuser',
@@ -153,6 +153,8 @@ class UserAPITestCase(APITestCase):
             role= User.Role.STUDENT
         )
         
+        self.url = reverse('user-list')
+        
     def test_admin_can_get_users_list(self):
         self.client.force_authenticate(user=self.admin)
         response = self.client.get(self.url)
@@ -172,11 +174,134 @@ class UserAPITestCase(APITestCase):
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
     
-    
-class CourseAPITestCase(APITestCase):
-    def setUp(self):
-        self.url = reverse('course-list')
+class UserDetailAPITestCase(APITestCase):
+    def setUp(self):     
+        self.admin = User.objects.create_user(
+            username= 'adminuser',
+            email= 'admin@strixhaven.com',
+            password= 'AdminPassword1',
+            role= User.Role.ADMIN
+        )
         
+        self.teacher = User.objects.create_user(
+            username= 'profvayran',
+            email= 'vayran@strixhaven.com',
+            password= 'Prismani',
+            role= User.Role.TEACHER
+        )
+        
+        self.student = User.objects.create_user(
+            username= 'noskzarrosh',
+            email= 'nossk@strixhaven.com',
+            password= 'NineInchClaws',
+            role= User.Role.STUDENT
+        )
+        
+        self.user = User.objects.create(
+            username = 'aidoneus',
+            email = 'aidoneus@strixhaven.com',
+            password = 'Odette!69',
+            role = User.Role.STUDENT
+        )
+        
+        self.url = reverse('user-detail', kwargs={'pk': self.user.pk})
+        
+    #GET request tests
+    def test_admin_can_get_a_user(self):
+        self.client.force_authenticate(user=self.admin)
+        response = self.client.get(self.url)
+        
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['username'], self.user.username)
+    
+    def test_teacher_can_not_get_a_user(self):
+        self.client.force_authenticate(user=self.teacher)
+        response = self.client.get(self.url)
+        
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+    
+    def test_student_can_not_get_a_user(self):
+        self.client.force_authenticate(user=self.student)
+        response = self.client.get(self.url)
+        
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+    
+    def test_unauthorized_can_not_access(self):
+        response = self.client.get(self.url)
+        
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        
+    def test_admin_can_not_get_nonexistent_user(self):
+        self.client.force_authenticate(user=self.admin)
+        url = reverse('user-detail', kwargs={'pk': 300})
+        response = self.client.get(url)
+        
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+    
+    #PATCH request tests    
+    def test_admin_can_update_a_user(self):
+        self.client.force_authenticate(user=self.admin)
+        data = {
+            'username': 'alexander',
+            'email': 'alexander@strixhaven.com',
+            'role': User.Role.TEACHER
+            }
+        response = self.client.patch(self.url, data, format='json')
+        
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.user.refresh_from_db()
+        self.assertEqual(self.user.username, 'alexander')
+        
+    def test_teacher_can_not_update_a_user(self):
+        self.client.force_authenticate(user=self.teacher)
+        data = {'email': 'alexander@strixhaven.com'}
+        response = self.client.patch(self.url, data, format='json')
+        
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        
+    def test_student_can_not_update_a_user(self):
+        self.client.force_authenticate(user=self.student)
+        data = {'role': User.Role.TEACHER}
+        response = self.client.patch(self.url, data, format='json')
+        
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+    
+    def test_unauthorized_can_not_update_user(self):
+        data = {'username': 'alexander'}
+        response = self.client.patch(self.url, data)
+        
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+    
+    #DELETE request tests
+    def test_admins_can_delete_a_user(self):
+        self.client.force_authenticate(user=self.admin)
+        response = self.client.delete(self.url)
+        
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertFalse(User.objects.filter(pk=self.user.pk).exists())
+        
+    def test_teacher_can_not_delete_a_user(self):
+        self.client.force_authenticate(user=self.teacher)
+        response = self.client.delete(self.url)
+        
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertTrue(User.objects.filter(pk=self.user.pk).exists())
+        
+    def test_student_can_not_delete_a_user(self):
+        self.client.force_authenticate(user=self.student)
+        response = self.client.delete(self.url)
+        
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertTrue(User.objects.filter(pk=self.user.pk).exists())
+    
+    def test_unauthorized_can_not_delete_user(self):
+        response = self.client.delete(self.url)
+        
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        
+        
+class CourseListAPITestCase(APITestCase):
+    def setUp(self):        
         self.admin = User.objects.create_user(
             username= 'adminuser',
             email= 'admin@strixhaven.com',
@@ -208,10 +333,13 @@ class CourseAPITestCase(APITestCase):
             "course_title" : "Magic Fundamentals of All Eight Schools",
             "course_description" : "Students will go over the foundations of magic, learn to meditate and will receive an increased Mana Pool."
         }    
+        
+        self.url = reverse('course-list')
                    
     def create_course(self, payload):
         return self.client.post(self.url, payload, format='json')
-        
+
+    #GET request tests        
     def test_registered_users_can_get_course_list(self):
         self.client.force_authenticate(user= self.admin)
         response = self.client.get(self.url)
@@ -223,6 +351,7 @@ class CourseAPITestCase(APITestCase):
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
     
+    #POST request tests
     def test_admin_can_create_course(self):
         self.client.force_authenticate(user= self.admin)
         response = self.create_course(self.valid_course_payload)   
@@ -233,7 +362,7 @@ class CourseAPITestCase(APITestCase):
     def test_teacher_can_create_course(self):
         self.client.force_authenticate(user= self.teacher)
         response = self.create_course(self.valid_course_payload)
-        
+    
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
     
     def test_student_can_create_course(self):
@@ -247,6 +376,150 @@ class CourseAPITestCase(APITestCase):
         
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)  
   
+  
+# get, put, delete course tests  url name course-detail
+class CourseDetailAPITestCase(APITestCase):
+    def setUp(self):     
+        self.admin = User.objects.create_user(
+            username= 'adminuser',
+            email= 'admin@strixhaven.com',
+            password= 'AdminPassword1',
+            role= User.Role.ADMIN
+        )
+        
+        self.teacher = User.objects.create_user(
+            username= 'profvayran',
+            email= 'vayran@strixhaven.com',
+            password= 'Prismani',
+            role= User.Role.TEACHER
+        )
+        
+        self.other_teacher = User.objects.create_user(
+            username= 'proflisette',
+            email= 'lisette@strixhaven.com',
+            password= 'WitherBloom',
+            role= User.Role.TEACHER
+        )
+        
+        self.student = User.objects.create_user(
+            username= 'noskzarrosh',
+            email= 'nossk@strixhaven.com',
+            password= 'NineInchClaws',
+            role= User.Role.STUDENT
+        )
+        
+        self.course = Course.objects.create(
+            course_title= 'Flame of Knowledge',
+            course_description= 'Students will learn details events of Aracvios history.',
+            teacher=self.teacher
+        )
+        
+        self.url = reverse('course-detail', kwargs={'pk': self.course.pk})
+        
+    #GET request tests
+    def test_authorized_user_can_get_a_course(self):
+        self.client.force_authenticate(user=self.admin)
+        response = self.client.get(self.url)
+        
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['course_title'], self.course.course_title)
+    
+    def test_unauthorized_can_not_access(self):
+        response = self.client.get(self.url)
+        
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        
+    def test_authorized_user_can_not_get_nonexistent_course(self):
+        self.client.force_authenticate(user=self.admin)
+        url = reverse('course-detail', kwargs={'pk': 300})
+        response = self.client.get(url)
+        
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+    
+    #PUT request tests    
+    def test_admin_can_update_a_course(self):
+        self.client.force_authenticate(user=self.admin)
+        data = {
+            'course_title': 'Speaking the Same Language',
+            'course_description': 'Students will learn how to solve common magical cyphers or use magic to comprehend text that is alien to them.',
+            }
+        response = self.client.put(self.url, data, format='json')
+        
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.course.refresh_from_db()
+        self.assertEqual(self.course.course_title, 'Speaking the Same Language')
+        
+    def test_teacher_can_update_a_course(self):
+        self.client.force_authenticate(user=self.teacher)
+        data = {
+            'course_title': 'Flame of Knowledge',
+            'course_description': 'Students will learn details events of Aracvios history, or may spontaneously gain the breath of a Dragon.',
+            }
+        response = self.client.put(self.url, data, format='json')
+        
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.course.refresh_from_db()
+        self.assertEqual(self.course.course_description, 'Students will learn details events of Aracvios history, or may spontaneously gain the breath of a Dragon.')
+        
+    def test_teacher_can_not_update_other_teachers_course(self):
+        self.client.force_authenticate(user= self.other_teacher)
+        data= {
+            'course_title': 'S.T.E.M: Leafbinding for Beginners',
+            'course_description': 'Students will either learn how to use magic to soothe and heal a wound or how to commune with Flora'
+        }
+        response = self.client.put(self.url, data, format= 'json')
+        
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        
+    def test_student_can_not_update_a_course(self):
+        self.client.force_authenticate(user=self.student)
+        data = {
+            'course_title': 'Lame Knowledge',
+            'course_description': 'Learn about some boring old history.'
+            }
+        response = self.client.put(self.url, data, format='json')
+        
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+    
+    def test_unauthorized_can_not_update_user(self):
+        data = {'course_description': 'There is no course.'}
+        response = self.client.put(self.url, data)
+        
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+    
+    #DELETE request tests
+    def test_admins_can_delete_a_course(self):
+        self.client.force_authenticate(user=self.admin)
+        response = self.client.delete(self.url)
+        
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertFalse(Course.objects.filter(pk=self.course.pk).exists())
+        
+    def test_teacher_can_delete_a_course(self):
+        self.client.force_authenticate(user=self.teacher)
+        response = self.client.delete(self.url)
+        
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertFalse(Course.objects.filter(pk=self.course.pk).exists())
+        
+    def test_student_can_not_delete_a_course(self):
+        self.client.force_authenticate(user=self.student)
+        response = self.client.delete(self.url)
+        
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertTrue(Course.objects.filter(pk=self.course.pk).exists())
+    
+    def test_unauthorized_can_not_delete_course(self):
+        response = self.client.delete(self.url)
+        
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+# get for logged in students courses url name enrolled-courses
+
+# get list of courses created by that teacher url name teacher-courses
+
+# post enrolls a student in a course url name enroll-student
+
 # class is your describe block
 # Arrange (set up data), Act (make request), Assert (check results/responses)
 # Each test in a class block must make their own request, query their own user and make their own assertions
